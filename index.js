67b2c2e9fc6963f59f509aa355858715ed72581b
Mike Sands <mikesands94@gmail.com>
2022-04-25 21:14:40 -0400

    finished questions function

--- index.js
+++ index.js
@@ -37,4 +37,61 @@ const questions = async () => {
             choices: ['Manager', 'Engineer', 'Intern'],
             },
         ])
-}
+
+
+// If manager, answer this question
+    if (answers.role === 'Manager') {
+        const managerRole = await inquirer
+            .prompt([
+            {
+                type:'input',
+                message: 'What is your office number?',
+                name: 'officeNumber',
+            },
+            ])
+            const newManager = new Manager (
+                answers.name,
+                answers.id,
+                answers.email,
+                managerRole.officeNumber
+            );
+            newStaffInfo.push(newManager);
+
+// If engineer, answer this question
+    } else if (answers.role === 'Engineer'){
+        const engineerRole = await inquirer
+            .prompt ([
+                {
+                    type: 'input',
+                    message: 'What is your GitHub username?',
+                    name: 'github',
+                }
+            ])
+            const newEngineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                engineerRole.github
+            );
+            newStaffInfo.push(newEngineer);
+    
+
+// If intern, answer this question
+    } else if (answers.role === 'Intern') {
+        const internRole = await inquirer
+            .prompt ([
+                {
+                    type: 'input',
+                    message: 'What school did you attend?',
+                    name: 'school',
+                },
+            ])
+            const newIntern = new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                internRole.school
+            );
+            newStaffInfo.push(newIntern);
+    }
+};
