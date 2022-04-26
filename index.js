5cc1bc38eb7229a1ec98e572aabbd7bfaf1d93af
Mike Sands <mikesands94@gmail.com>
2022-04-25 20:54:55 -0400

    added prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Node packages
 const inquirer = require ('inquirer');
 const fs = require ('fs');
-
+const generateStaff = require ('./src/template-helper')
 
 // Lib classes
 const Engineer = require ('./lib/Engineer');
@@ -10,3 +10,31 @@ const Manager = require ('./lib/Manager');
 
 // Array for answers
 const newStaffInfo = [];
+
+// Questions to be asked in command line to user
+const questions = async () => {
+    const answers = await inquirer
+        .prompt ([
+            {
+            type: 'input',
+            message: 'What is your name?', 
+            name: 'name',
+            },
+            {
+            type: 'input',
+            message: 'What is your ID number?', 
+            name: 'id',
+            },
+            {
+            type: 'input',
+            message: 'What is your email address?', 
+            name: 'email',
+            },
+            {
+            type: 'list',
+            message: 'What is your role?', 
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern'],
+            },
+        ])
+}
